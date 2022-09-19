d7d7e54fbeb43a5d2c946cade8689476beb58a62
Leonardo Rick <leonardo@lanytecnologia.com.br>
2022-09-19 02:45:50 +0100

    create tests for document-events and main

--- documents-page/tests/document-events.test.js
+++ documents-page/tests/document-events.test.js
@@ -2,11 +2,41 @@ import DocumentEvents from '../src/document-events';
 
 describe('DocumentEvents', () => {
     let documentEvents;
+    let div;
+
+    beforeEach(() => {
+        documentEvents = new DocumentEvents();
+        div = document.createElement('div');
+        document.body.appendChild(div);
+    });
 
     it('should create', () => {
         const addEventListener = jest.spyOn(document, 'addEventListener');
         documentEvents = new DocumentEvents();
         expect(addEventListener).toHaveBeenCalledTimes(2);
         expect(documentEvents).toBeTruthy();
+    });
+
+    it('shold', () => {
+        const dp = jest.spyOn(window, 'dispatchEvent');
+        const input = document.createElement('input');
+        input.setAttribute('id', 'sortSelect');
+        document.body.appendChild(input);
+        input.dispatchEvent(new Event('input', { bubbles: true, cancelable: true }));
+        expect(dp).toHaveBeenCalled();
+    });
+
+    it('should call dispatchEvent when element with id "view-list" is clicked', () => {
+        const dp = jest.spyOn(window, 'dispatchEvent');
+        div.setAttribute('id', 'view-list');
+        div.click();
+        expect(dp).toHaveBeenCalled();
+    });
+
+    it('should call dispatchEvent when elemtn with id "view-grid" is clicked', () => {
+        const dp = jest.spyOn(window, 'dispatchEvent');
+        div.setAttribute('id', 'view-grid');
+        div.click();
+        expect(dp).toHaveBeenCalled();
     });
 });
--- documents-page/tests/main.test.js
+++ documents-page/tests/main.test.js
@@ -1,13 +1,54 @@
-import API from '../src/api';
 import Main from '../src/main';
 
 jest.mock('../src/api.js');
-// jest.mock;
-
+jest.mock('../src/ui-updates.js');
 describe('Main', () => {
     let main;
-    it('should create', () => {
+
+    beforeEach(() => {
         main = new Main();
+    });
+
+    it('should create', () => {
         expect(main).toBeTruthy();
+    });
+
+    it('should init', () => {
+        const fetchDocuments = jest.spyOn(main, 'fetchDocuments');
+        const setEventListeners = jest.spyOn(main, 'setEventListeners');
+        const setNotifications = jest.spyOn(main, 'setNotifications');
+        main.init();
+        expect(fetchDocuments).toHaveBeenCalled();
+        expect(setEventListeners).toHaveBeenCalled();
+        expect(setNotifications).toHaveBeenCalled();
+    });
+
+    it('should call sortDocuments() when SELECT_CHANGE is emmited', () => {
+        const sortDocuments = jest.spyOn(main.uiUpdates, 'sortDocuments');
+        main.setEventListeners();
+        dispatchEvent(
+            new CustomEvent('select-change', {
+                detail: 'title',
+            })
+        );
+        expect(sortDocuments).toHaveBeenCalled();
+    });
+
+    it('should call changeDocumentsView() when CHANGE_VIEW is emmited', () => {
+        const changeDocumentsView = jest.spyOn(main.uiUpdates, 'changeDocumentsView').mockImplementation();
+        main.setEventListeners();
+        dispatchEvent(
+            new CustomEvent('change-view', {
+                detail: 'view-list',
+            })
+        );
+        expect(changeDocumentsView).toHaveBeenCalled();
+    });
+
+    it('should call addDocumentsError when a error happens during documents fetch', async () => {
+        jest.spyOn(main.api, 'getDocuments').mockImplementation(() => Promise.reject('something went wrong'));
+        const addDocumentsError = jest.spyOn(main.uiUpdates, 'addDocumentsError');
+        await main.fetchDocuments();
+        expect(addDocumentsError).toHaveBeenCalled();
     });
 });
